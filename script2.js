3aac401024577ffdadf4b3c88bdfd8a34d0a414d
attrenate <nikolozghomidzevol100@gmail.com>
2025-03-08 15:02:57 +0400

    practising 'onclick'

---
+++ script2.js
@@ -0,0 +1,8 @@
+// with let it's possible to change the variable.
+let cartQuanity = 0;
+// with const we cannot change the variables meaning.
+const variable2 = 'Hello';
+// for sum reason we don't use var no more )) 
+var variable3 = 1;
+console.log(typeof variable2);
+console.log(typeof cartQuanity);
