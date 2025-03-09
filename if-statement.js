b7915f45832c1c4bfbab8e3f3718921c22053536
attrenate <nikolozghomidzevol100@gmail.com>
2025-03-09 14:30:29 +0400

    sum more practise

--- if-statement.js
+++ if-statement.js
@@ -25,3 +25,30 @@ if (age > discount) {
 } else {
     console.log('You get the discount');
 }
+
+// coin-flip game
+let randomNumber = Math.random();
+let result = '';
+
+
+if(randomNumber >= 0.5){
+    result = 'head';
+}else{
+    result = 'tails';
+}
+
+// Coin flip game - guessing the result
+let guess = prompt('Guess the result (head/tails): ').toLowerCase(); // 
+
+// Generate a random coin flip result
+let randomNumber1 = Math.random();
+let result1 = randomNumber1 >= 0.5 ? 'head' : 'tails';
+
+console.log(`Coin flip result: ${result1}`);
+
+// Check if the guess is correct
+if (guess === result1) {
+    console.log('You win!');
+} else {
+    console.log('You lose!');
+}
