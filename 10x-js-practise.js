3d5c9ba251421654c1e8366ab3ec08fb3ee6572e
attrenate <nikolozghomidzevol100@gmail.com>
2025-03-12 14:34:42 +0400

    initial commit

---
+++ 10x-js-practise.js
@@ -0,0 +1,13 @@
+alert('Welcome to JavaScript');
+
+function changeColor(){
+    document.body.style.backgroundColor = 'red';
+    console.log('Changed Color.');
+    console.log('chais adughebA yo yo yo');
+    console.log('chais adughebA yo yo yo');
+    console.log('chais adughebA yo yo yo');
+    console.log('chais adughebA yo yo yo');
+    console.log('chais adughebA yo yo yo');
+    console.log('chais adughebA yo yo yo');
+}
+setTimeout(changeColor, 3000);
